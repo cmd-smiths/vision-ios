902dd39ab9cd59a8ecc8322034ca6707bfed9aca
88HOMIN <88homin@gmail.com>
2024-08-10 01:15:19 +0900

    init

--- Vison/VisonApp.swift
+++ Vison/VisonApp.swift
@@ -4,14 +4,19 @@
 //
 //  Created by HOMIN on 8/10/24.
 //
-
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct VisonApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(
+                store: Store(initialState: CounterFeature.State()) {
+                    CounterFeature()
+                        ._printChanges()
+                }
+            )
         }
     }
 }
