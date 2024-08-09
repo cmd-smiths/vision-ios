fc1e95b3bf63ab50d1f8920a404dd1028d1e4f06
88homin <88homin@gmail.com>
2024-08-10 00:28:16 +0900

    Initial Commit

---
+++ Vison/VisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisonApp.swift
+//  Vison
+//
+//  Created by HOMIN on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
